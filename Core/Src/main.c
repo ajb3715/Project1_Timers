a929bc821a3e61cac4e4be17b04b3443e8e64f35
Alex Murphy <amurph0602@icloud.com>
2024-02-15 15:08:59 -0500

    prints and inputs set up

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,6 +5,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <uart.h>
 
@@ -15,6 +16,10 @@
 volatile uint32_t currentTick = 0;
 volatile uint32_t startTick = 0;
 volatile bool pulseDetected = false;
+volatile uint32_t lowerLimit = 450;
+volatile uint32_t upperLimit = 550;
+uint8_t buffer[100];
+int n;
 
 // runs the power on self-test. Returns true if the test passes, false otherwise
 //confirm that the GPIO port is seeing pulses at least once in 100 milliseconds
@@ -24,8 +29,17 @@ _Bool power_on_self_test( void );
 int set_timer_base( void );
 
 // Captures 1 line of text from console. Returns nul terminated string when \n is entered
-void get_line ( void *buffer, int max_length );
+void get_line ( uint8_t *buffer, int max_length );
 
+void printString(char* string){
+	n = sprintf((char *)buffer, "%s", string);
+	USART_Write(USART2, buffer, n);
+}
+
+void printChar(char ch){
+	n = sprintf((char *)buffer, "%c", ch);
+	USART_Write(USART2, buffer, n);
+}
 
 
 //////////////////////////////////////////////////////////////
@@ -36,22 +50,78 @@ void get_line ( void *buffer, int max_length );
 int main(void) {
 
     // Initialization executed once at startup
-    UART_Init();
-    TIM_Init();
-
-    while( power_on_self_test() == false)
-        ;
+//    UART_Init();
+//    TIM_Init();
+	USART2_Init(9600);			// Initialize USART for terminal
+	clock_init();				// Initialize clock
+//    while( power_on_self_test() == false)
+//        ;
+	uint8_t command[5];			// buffer for input command
 
     // Main loop runs forever
     while(1)
     {
     	// 1. Print “Enter expected period or <CR> if no change”. Wait for user response
-    	print(message);
-    	get_line(buffer, sizeof(buffer));
+
+    	printString("\r\nWould you like to use the default limits:\r\nLower: 450\r\nUpper: 550\r\n");
+    	char rx = USART_Read(USART2);
+    	int i = 0;
+
+    	while (rx != '\r'){										// loop waits for the return command before processing input
+    		printChar(rx);
+			command[i] = rx;									// adds input to buffer to process command
+			i++;
+			rx = USART_Read(USART2);							// reads next character
+    	}
+    	command[i] = '\0';										// terminates inputed command
+
+    	for (int index = 0; index < i; index++){
+    		command[index] = tolower(command[index]);			// makes command case insensitive
+    	}
+
+
+    	if (strcmp("yes", (char *) command)){					// checks for custom limits
+    		printString("\r\nWhat would you like the lower limit to be? (must be between 50 and 950): ");
+    		rx = USART_Read(USART2);
+    		i = 0;
+
+    		while (rx != '\r'){									// loop waits for the return command before processing input
+				printChar(rx);
+				command[i] = rx;								// adds input to buffer to process input limit
+				i++;
+				rx = USART_Read(USART2);						// reads next character
+    		}
+    		command[i] = '\0';									// terminates inputed string for processing
+    		i = 0;
+    		lowerLimit = atoi((char *)command);					// converts string of int into int for use as limit
+
+			while (lowerLimit < 50 || lowerLimit > 950){
+
+				printString("\r\nLower limit must be between 50 and 950. Please enter a new limit: ");
+				rx = USART_Read(USART2);
+
+				while (rx != '\r'){									// loop waits for the return command before processing input
+					printChar(rx);
+					command[i] = rx;								// adds input to buffer to process input limit
+					i++;
+					rx = USART_Read(USART2);						// reads next character
+				}
+
+		   		command[i] = '\0';									// terminates inputed string for processing
+		   		i = 0;
+		   		lowerLimit = atoi((char *)command);					// converts string of int into int for use as limit
+			}
+			upperLimit = lowerLimit + 100;
+			printString((char *) printf("\r\nLower limit: %lu, Upper limit: %lu", lowerLimit, upperLimit));
+			USART_Write(USART2, buffer, n);
+    	}
+    	printString("\r\n");					// starts a new line (since input may have been last thing to print)
+
+    	printString("Limits are currently set, program can run to calculate\r\n\r\n");
 
     	// 2. if yes, read new period then set up timer clock
-        while( set_timer_base() == 0 )
-            ;
+//        while( set_timer_base() == 0 )
+//            ;
 
         // 3. read 100 pulses
 
