2d56128a42d828debf3d852342542426d251abbf
alexb <ajb3715@g.rit.edu>
2024-02-20 09:35:17 -0500

    Cleaned up Code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,12 +36,98 @@ int dutys[1001];
 _Bool power_on_self_test( void );
 
 // ask user for expected period, sets timer clock accordingly. Return period or 0 if invalid
-int set_timer_base( void ){
-	return lowerLimit;
-}
+int set_timer_base( void );
 
 // Captures 1 line of text from console. Returns nul terminated string when \n is entered
 void get_line ( uint8_t *buffer, int max_length );
+
+void printString(char* string);
+
+struct KeyValue* initialize_map(int size);
+
+void update_map(struct KeyValue* map, int* numbers, int size);
+
+
+void swap(struct KeyValue* a, struct KeyValue* b);
+
+void sort_dictionary(struct KeyValue* arr, size_t arrSize);
+
+void printChar(char ch);
+
+_Bool power_on_self_test(void);
+
+uint32_t TIM2_EdgeToEdge(void);
+
+int init_measurement( uint32_t limit );
+
+void make_measurements( uint32_t limit );
+
+
+void printFunct(char* printBuffer);
+
+void print_measurements( uint32_t limit );
+
+void print_duty();
+
+
+void Start_Timer(void);
+
+void Stop_Timer(void);
+
+void TIM_Duty(void);
+
+void UserInputs(void);
+
+
+//////////////////////////////////////////////////////////////
+// Embedded code usually consists of 2 components
+//  - The init section is run once at startup and initializes all low level drivers and modules
+//  - A main loop that runs forever that calls the application tasks repeatedly.
+////////////////
+int main(void) {
+
+    // Initialization executed once at startup
+//    UART_Init();
+    TIM_Init();
+	USART2_Init(9600);			// Initialize USART for terminal
+	clock_init();				// Initialize clock
+	Start_Timer();
+    while( power_on_self_test() == false)
+        ;
+
+
+    // Main loop runs forever
+    while(1)
+    {
+
+    	//Get User input
+    	UserInputs();
+
+        while( set_timer_base() == 0 )
+          ;
+
+        //Collect Period Data
+        Start_Timer();
+        init_measurement(lowerLimit);
+    	make_measurements(lowerLimit);
+    	print_measurements(lowerLimit);
+    	Stop_Timer();
+
+    	//Collect Duty Data
+    	Start_Timer();
+    	TIM_Duty();
+    	Stop_Timer();
+    	print_duty();
+
+
+        // 4. print out results
+
+    }
+}
+
+int set_timer_base( void ){
+	return lowerLimit;
+}
 
 void printString(char* string){
 	n = sprintf((char *)buffer, "%s", string);
@@ -89,6 +175,7 @@ void update_map(struct KeyValue* map, int* numbers, int size) {
         }
     }
 }
+
 /*A helper function that swaps to entries in a dictionary
  *
  */
@@ -97,6 +184,7 @@ void swap(struct KeyValue* a, struct KeyValue* b) {
     *a = *b;
     *b = temp;
 }
+
 /*Sorts the dictionary in ascending order
  *
  */
@@ -164,7 +252,6 @@ void make_measurements( uint32_t limit ) {
 	  }
 }
 
-
 void printFunct(char* printBuffer) {
 	USART_Write(USART2, (uint8_t*)printBuffer, strlen(printBuffer)); // simple print solution
 }
@@ -175,6 +262,9 @@ void print_measurements( uint32_t limit ) {
 	  update_map(Measures, meas, 1000);
 	  //Sort the dictionary in ascending order
 	  sort_dictionary(Measures, 1000);
+
+	  sprintf(lineBuffer, "Period Calculation\r\n");
+	  printFunct(lineBuffer);
 
 	for (int i = 0; i < 1001; i++) {
 		if ((Measures[i].key > (lowerLimit)) && (Measures[i].key < (lowerLimit + 101))) {
@@ -203,13 +293,13 @@ void print_duty(){
 			  sprintf(lineBuffer, "\r\n");
 			  printFunct(lineBuffer);
 			  sprintf(lineBuffer, "Duty Cycle Calculation\r\n");
+			  printFunct(lineBuffer);
 			  sprintf(lineBuffer, " Duty: %d Count: %d \r\n",DutyCycle, count );
 			  printFunct(lineBuffer);
 		  }
 		  index++;
 	  }
 }
-
 
 void Start_Timer(void) {
     // Start the timer by enabling the counter
@@ -256,11 +346,17 @@ void TIM_Duty(void){
 	    		  if((tFall != 0.0) && (tRise != 0.0) && (tEnd != 0.0)){
 	    			  //double tOn = tFall - tRise;
 	    			  //double tOff = tEnd - tFall;
+	    			  double prevDuty = .5;
 	    			  double DutyCyc = tFall / tRise;
 	    			  //We throw out the first measurement because it hasnt seen the next pulse yet
 	    			  if(ind != 0){
+	    				  if(DutyCyc > (prevDuty + .01)){
+	    					  DutyCyc = prevDuty;
+	    				  }
+	    			  prevDuty = DutyCyc;
 	    			  dutys[ind] = DutyCyc * 100;
 	    			  }
+
 	    			  //reset the fall and end times
 	    			  tFall = 0.0;
 	    			  tRise = tEnd;
@@ -277,110 +373,70 @@ void TIM_Duty(void){
 	    		  }
 	    	  }
 	    	  ind = 0;
+	    	  if((dutys[1] > dutys[2]) || dutys[1] < dutys[2]){
+	    		  dutys[1] = dutys[2];
+	    	  }
 }
 
-
-//////////////////////////////////////////////////////////////
-// Embedded code usually consists of 2 components
-//  - The init section is run once at startup and initializes all low level drivers and modules
-//  - A main loop that runs forever that calls the application tasks repeatedly.
-////////////////
-int main(void) {
-
-    // Initialization executed once at startup
-//    UART_Init();
-    TIM_Init();
-	USART2_Init(9600);			// Initialize USART for terminal
-	clock_init();				// Initialize clock
-	Start_Timer();
-    while( power_on_self_test() == false)
-        ;
+void UserInputs(void){
 	uint8_t command[5];			// buffer for input command
+	// 1. Print “Enter expected period or <CR> if no change”. Wait for user response
 
-    // Main loop runs forever
-    while(1)
-    {
-    	// 1. Print “Enter expected period or <CR> if no change”. Wait for user response
+	    	printString("\r\nWould you like to use the default limits:\r\nLower: 450\r\nUpper: 550\r\n");
+	    	char rx = USART_Read(USART2);
+	    	int i = 0;
 
-    	printString("\r\nWould you like to use the default limits:\r\nLower: 450\r\nUpper: 550\r\n");
-    	char rx = USART_Read(USART2);
-    	int i = 0;
-
-    	while (rx != '\r'){										// loop waits for the return command before processing input
-    		printChar(rx);
-			command[i] = rx;									// adds input to buffer to process command
-			i++;
-			rx = USART_Read(USART2);							// reads next character
-    	}
-    	command[i] = '\0';										// terminates inputed command
-
-    	for (int index = 0; index < i; index++){
-    		command[index] = tolower(command[index]);			// makes command case insensitive
-    	}
-
-
-    	if (strcmp("yes", (char *) command)){					// checks for custom limits
-    		printString("\r\nWhat would you like the lower limit to be? (must be between 50 and 9950): ");
-    		rx = USART_Read(USART2);
-    		i = 0;
-
-    		while (rx != '\r'){									// loop waits for the return command before processing input
-				printChar(rx);
-				command[i] = rx;								// adds input to buffer to process input limit
+	    	while (rx != '\r'){										// loop waits for the return command before processing input
+	    		printChar(rx);
+				command[i] = rx;									// adds input to buffer to process command
 				i++;
-				rx = USART_Read(USART2);						// reads next character
-    		}
-    		command[i] = '\0';									// terminates inputed string for processing
-    		i = 0;
-    		lowerLimit = atoi((char *)command);					// converts string of int into int for use as limit
+				rx = USART_Read(USART2);							// reads next character
+	    	}
+	    	command[i] = '\0';										// terminates inputed command
 
-			while (lowerLimit < 50 || lowerLimit > 9950){
+	    	for (int index = 0; index < i; index++){
+	    		command[index] = tolower(command[index]);			// makes command case insensitive
+	    	}
 
-				printString("\r\nLower limit must be between 50 and 950. Please enter a new limit: ");
-				rx = USART_Read(USART2);
 
-				while (rx != '\r'){									// loop waits for the return command before processing input
+	    	if (strcmp("yes", (char *) command)){					// checks for custom limits
+	    		printString("\r\nWhat would you like the lower limit to be? (must be between 50 and 9950): ");
+	    		rx = USART_Read(USART2);
+	    		i = 0;
+
+	    		while (rx != '\r'){									// loop waits for the return command before processing input
 					printChar(rx);
 					command[i] = rx;								// adds input to buffer to process input limit
 					i++;
 					rx = USART_Read(USART2);						// reads next character
+	    		}
+	    		command[i] = '\0';									// terminates inputed string for processing
+	    		i = 0;
+	    		lowerLimit = atoi((char *)command);					// converts string of int into int for use as limit
+
+				while (lowerLimit < 50 || lowerLimit > 9950){
+
+					printString("\r\nLower limit must be between 50 and 950. Please enter a new limit: ");
+					rx = USART_Read(USART2);
+
+					while (rx != '\r'){									// loop waits for the return command before processing input
+						printChar(rx);
+						command[i] = rx;								// adds input to buffer to process input limit
+						i++;
+						rx = USART_Read(USART2);						// reads next character
+					}
+
+			   		command[i] = '\0';									// terminates inputed string for processing
+			   		i = 0;
+			   		lowerLimit = atoi((char *)command);					// converts string of int into int for use as limit
 				}
+				upperLimit = lowerLimit + 100;
+				printString((char *) printf("\r\nLower limit: %lu, Upper limit: %lu", lowerLimit, upperLimit));
+				USART_Write(USART2, buffer, n);
+	    	}
+	    	printString("\r\n");					// starts a new line (since input may have been last thing to print)
 
-		   		command[i] = '\0';									// terminates inputed string for processing
-		   		i = 0;
-		   		lowerLimit = atoi((char *)command);					// converts string of int into int for use as limit
-			}
-			upperLimit = lowerLimit + 100;
-			printString((char *) printf("\r\nLower limit: %lu, Upper limit: %lu", lowerLimit, upperLimit));
-			USART_Write(USART2, buffer, n);
-    	}
-    	printString("\r\n");					// starts a new line (since input may have been last thing to print)
-
-    	printString("Limits are currently set, program can run to calculate\r\n\r\n");
-
-    	// 2. if yes, read new period then set up timer clock
-
-        while( set_timer_base() == 0 )
-          ;
-
-        // 3. read 100 pulses
-        Start_Timer();
-        init_measurement(lowerLimit);
-    	make_measurements(lowerLimit);
-    	print_measurements(lowerLimit);
-    	Stop_Timer();
-
-
-    	Start_Timer();
-    	TIM_Duty();
-    	Stop_Timer();
-    	print_duty();
-
-
-        // 4. print out results
-
-    }
+	    	printString("Limits are currently set, program can run to calculate\r\n\r\n");
 }
-
 
 
